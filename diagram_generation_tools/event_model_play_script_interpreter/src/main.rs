8c8a7ffa05b8c4738ccb08acb3bbc1285e7f38fd
fullstackgjj <fullstackgjj@gmail.com>
2021-02-22 23:00:02 -0500

    Updated program to use CLI tools

--- diagram_generation_tools/event_model_play_script_interpreter/src/main.rs
+++ diagram_generation_tools/event_model_play_script_interpreter/src/main.rs
@@ -1,19 +1,26 @@
-// Add cool slogan for your app here, e.g.:
-/// Get first n lines of a file
+use quicli::prelude::*;
+use structopt::StructOpt;
+
+
+/// Load event model play script to generate specified output
 #[derive(Debug, StructOpt)]
 struct Cli {
-    // Add a CLI argument `--count`/-n` that defaults to 3, and has this help text:
-    /// How many lines to get
-    #[structopt(long = "count", short = "n", default_value = "3")]
-    count: usize,
-    // Add a positional argument that the user has to supply:
+
     /// The file to read
     file: String,
-    // Quick and easy logging setup you get for free with quicli
+
+    /// Which perspective to filter
+    #[structopt(long = "filter", short = "f", default_value = "")]
+    filter: String,
+
     #[structopt(flatten)]
     verbosity: Verbosity,
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> CliResult {
+    let args = Cli::from_args();
+    read_file(&args.file)?
+        .lines()
+        .for_each(|line| println!("{}", line));
+    Ok(())
 }
