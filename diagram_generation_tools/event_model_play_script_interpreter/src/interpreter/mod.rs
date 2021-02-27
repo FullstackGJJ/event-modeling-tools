6ce874367918af4b32b2b464f23e921d5d5681d3
fullstackgjj <fullstackgjj@gmail.com>
2021-02-27 11:57:31 -0500

    Saving work

--- diagram_generation_tools/event_model_play_script_interpreter/src/interpreter/mod.rs
+++ diagram_generation_tools/event_model_play_script_interpreter/src/interpreter/mod.rs
@@ -36,6 +36,49 @@ struct EventPlayScript {
     script: Script
 }
 
-pub fn parse_input_file() {
+type InputFile = String;
+
+type Filter = String;
+
+type UsersSection = Vec<String>;
+
+type SystemsSection = Vec<String>;
+
+type DataTypesSection = Vec<String>;
+
+type SettingSection = Vec<String>;
+
+type ScopeSection = Vec<String>;
+
+type ScriptSection = Vec<String>;
+
+type FileParseError = String;
+
+type PlayParseError = String;
+
+type FilterError = String;
+
+struct ValidEventPlayText {
+    users_section = UsersSection,
+    systems_section = SystemsSection,
+    data_types_section = DataTypesSection,
+    setting_section = SettingSection,
+    scope_section = ScopeSection,
+    script_section = ScriptSection
+}
+
+pub fn parse_input_file(inputFile: InputFile) -> Result<ValidEventPlayText, FileParseError> {
+
+}
+
+pub fn parse_valid_event_play_text :: ValidEventPlayText -> Result(EventPlayScript, PlayParseError) {
+
+}
+
+pub fn apply_filter :: EventPlayScript -> Filter -> Result(EventPlayScript, FilterError) {
+
+}
+
+pub fn get_event_play_script_text :: EventPlayScript -> ValidEventPlayText {
 
 }
