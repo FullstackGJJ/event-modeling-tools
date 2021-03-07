763bc7d09c561b1e62cb807d80fc1b58db2d6755
fullstackgjj <fullstackgjj@gmail.com>
2021-03-07 01:30:49 -0500

    added unit tests

--- diagram_generation_tools/event_model_play_script_interpreter_haskell/event-model-play-script-interpreter/src/Functions.hs
+++ diagram_generation_tools/event_model_play_script_interpreter_haskell/event-model-play-script-interpreter/src/Functions.hs
@@ -1,4 +1,4 @@
-module Functions ( Functions.filter ) where
+module Functions (Functions.filter) where
 
 import Data.Text
 import Data.Text.Conversions
@@ -8,55 +8,17 @@ import Internal.Script
 
 filter :: EventPlayScript -> FilterParameter -> EventPlayScript
 filter eventPlayScript filterParameter =
-    let script' = (script eventPlayScript)
-        filteredScript = hideUnnecessaryLines script' filterParameter
-        updatedUsers = filterDefinitionsUsingNewScript script' (users eventPlayScript)
-        updatedSystems = filterDefinitionsUsingNewScript script' (systems eventPlayScript)
-        updatedDataTypes = filterDataTypesUsingNewScript script' (dataTypes eventPlayScript)
+    let filteredScript = hideUnnecessaryLines (script eventPlayScript) filterParameter
+        updatedUsers = filterDefinitionsByScript filteredScript (users eventPlayScript) ActorName
+        updatedSystems = filterDefinitionsByScript filteredScript (systems eventPlayScript) ActorName
+        updatedDataTypes = filterDefinitionsByScript filteredScript (dataTypes eventPlayScript) LineContent
     in eventPlayScript { script = filteredScript
                        , users = updatedUsers
                        , systems = updatedSystems
                        , dataTypes = updatedDataTypes }
 
-filterDataTypesUsingNewScript :: Script -> [ DataType ] -> [ DataType ]
-filterDataTypesUsingNewScript script dataTypes =
-    Prelude.filter (scriptReferencesDataType script) dataTypes
+data SearchParameter = ActorName | LineContent
 
-scriptReferencesDataType :: Script -> DataType -> Bool
-scriptReferencesDataType script dataType =
-    Prelude.any (isDataTypeInsideActorScriptLine dataType) script
-
-isDataTypeInsideActorScriptLine :: DataType -> ActorScriptLine -> Bool
-isDataTypeInsideActorScriptLine dataType (HiddenLine hiddenSymbol) = False
-isDataTypeInsideActorScriptLine dataType (Line _ scriptLine) =
-    case scriptLine of (Event event) -> isDataTypeReferencedInEvent dataType event
-                       (Broadcast broadcast) -> isDataTypeReferencedInBroadcast dataType broadcast
-
-isDataTypeReferencedInEvent :: DataType -> Event -> Bool
-isDataTypeReferencedInEvent dataType (ReceivedBroadcast broadcast) = isDataTypeReferencedInBroadcast dataType broadcast
-isDataTypeReferencedInEvent dataType (Action actionContent) = isDataTypeReferencedInActionContent dataType actionContent
-
-isDataTypeReferencedInActionContent :: DataType -> ActionContent -> Bool
-isDataTypeReferencedInActionContent dataType actionContent =
-    if toText (name dataType) `isInfixOf` toText actionContent then True else False
-
-isDataTypeReferencedInBroadcast :: DataType -> Broadcast -> Bool
-isDataTypeReferencedInBroadcast dataType (REQUEST _ _ parameters ) =
-    if toText (name dataType) `isInfixOf` toText parameters then True else False
-isDataTypeReferencedInBroadcast dataType (RESPONSE _ _ response) =
-    if toText (name dataType) `isInfixOf` toText response then True else False
-
-filterDefinitionsUsingNewScript :: Script -> [ Definition ] -> [ Definition ]
-filterDefinitionsUsingNewScript script definitions =
-    Prelude.filter (scriptReferencesDefinition script) definitions
-
-scriptReferencesDefinition :: Script -> Definition -> Bool
-scriptReferencesDefinition script definition =
-    Prelude.any (isDefinitionInsideActorScriptLine definition) script
-
-isDefinitionInsideActorScriptLine :: Definition -> ActorScriptLine -> Bool
-isDefinitionInsideActorScriptLine definition (HiddenLine hiddenSymbol) = False
-isDefinitionInsideActorScriptLine definition (Line actor _) =
-    if toText (name definition) `isInfixOf` toText actor
-    then True
-    else False
+filterDefinitionsByScript :: Script -> [ Definition ] -> SearchParameter -> [ Definition ]
+filterDefinitionsByScript script definitions ActorName = Prelude.filter (\x -> (actorInScript script) (name x)) definitions
+filterDefinitionsByScript script word LineContent = Prelude.filter (\x -> (mentionedInScript script) (name x)) word
--- diagram_generation_tools/event_model_play_script_interpreter_haskell/event-model-play-script-interpreter/src/Internal/Script.hs
+++ diagram_generation_tools/event_model_play_script_interpreter_haskell/event-model-play-script-interpreter/src/Internal/Script.hs
@@ -31,13 +31,46 @@ type Script = [ ActorScriptLine ]
 
 type FilterParameter = String
 
+----------------------Public Functions------------------------------
 
 hideUnnecessaryLines :: Script -> FilterParameter -> Script
+actorInScript :: Script -> String -> Bool
+mentionedInScript :: Script -> String -> Bool
+
 hideUnnecessaryLines script filterParameter = 
     let lineFilter = filterLine filterParameter
     in Prelude.map lineFilter script 
 
+actorInScript script actor = Prelude.any (actorInLine actor) script
+
+mentionedInScript script word = Prelude.any (mentionedInLine word) script
+
+---------------------Private Functions------------------------------
+
+actorInLine :: String -> ActorScriptLine -> Bool
+mentionedInLine :: String -> ActorScriptLine -> Bool
 filterLine :: FilterParameter -> ActorScriptLine -> ActorScriptLine
+
+actorInLine actorName (HiddenLine hiddenSymbol) = False
+actorInLine actorName (Line actorInLine scriptLine) = 
+    if toText actorName `isInfixOf` toText actorInLine
+    then 
+        True
+    else
+        case scriptLine of (Event (ReceivedBroadcast (REQUEST actorReference _ _ ))) -> toText actorName `isInfixOf` toText actorReference
+                           (Event (ReceivedBroadcast (RESPONSE actorReference _ _))) -> toText actorName `isInfixOf` toText actorReference
+                           (Broadcast (REQUEST actorReference _ _ )) -> toText actorName `isInfixOf` toText actorReference
+                           (Broadcast (RESPONSE actorReference _ _)) -> toText actorName `isInfixOf` toText actorReference
+                           (Event _) -> False
+
+mentionedInLine word (HiddenLine hiddenSymbol) = False
+mentionedInLine word (Line _ scriptLine) =
+    case scriptLine of (Event (ReceivedBroadcast (REQUEST _ _ parameters ))) -> toText word `isInfixOf` toText parameters
+                       (Event (ReceivedBroadcast (RESPONSE _ _ response))) -> toText word `isInfixOf` toText response
+                       (Event (Action actionContent)) -> toText word `isInfixOf` toText actionContent
+                       (Broadcast (REQUEST _ _ parameters )) -> toText word `isInfixOf` toText parameters
+                       (Broadcast (RESPONSE _ _ response)) -> toText word `isInfixOf` toText response
+
 filterLine filterParameter actorScriptLine = 
     case actorScriptLine of (Line actor _) -> if toText actor `isInfixOf` toText filterParameter then actorScriptLine else HiddenLine "..."
                             (HiddenLine hiddenSymbol) -> HiddenLine hiddenSymbol
